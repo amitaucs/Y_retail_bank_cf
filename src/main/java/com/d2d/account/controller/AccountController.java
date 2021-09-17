c7d99a456030123e0bc94b099b93f6b8f7f7683b
ADATTA3 <amit.datta2@wipro.com>
2021-09-17 14:24:57 +0530

    First Commit

---
+++ src/main/java/com/d2d/account/controller/AccountController.java
@@ -0,0 +1,41 @@
+package com.d2d.account.controller;
+
+
+import io.swagger.annotations.*;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@AllArgsConstructor
+@RequestMapping("/retailBank/v1")
+@Tag(name = "New Account")
+@Api(tags = {"New Account"})
+public class AccountController {
+
+    @ApiOperation(value = "Greeting")
+    @ApiResponses(value = {@ApiResponse(code = 500, message = "Internal Server Error"),
+            @ApiResponse(code = 200, message = "OK", response = String.class)})
+
+    @GetMapping("/welcome")
+    public ResponseEntity<String> creditCardApplicationStatus(
+            @ApiParam(value = "Customer name", type = "String", required = true)
+            @RequestParam String customerName) {
+
+        if (StringUtils.isEmpty(customerName)) {
+            ResponseEntity.badRequest().build();
+        }
+
+        return  ResponseEntity.status(HttpStatus.OK).body("Hello "+customerName+ " welcome..");
+
+    }
+}
